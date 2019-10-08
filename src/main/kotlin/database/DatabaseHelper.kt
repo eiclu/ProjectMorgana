edde540d2ba49959b058301c49db237f8399c36f
Luca Eichler <mail@luca-eichler.at>
2019-10-08 20:52:00 +0200

    Fixed Caching

--- src/main/kotlin/database/DatabaseHelper.kt
+++ src/main/kotlin/database/DatabaseHelper.kt
@@ -13,151 +13,200 @@ import java.util.*
 
 class DatabaseHelper() {
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
-    private fun getDbConnection() = DriverManager.getConnection("jdbc:${System.getenv("DB_URL")}", System.getenv("DB_USERNAME"), System.getenv("DB_PASSWORD"))
+    private fun getDbConnection() = DriverManager.getConnection(
+        "jdbc:${System.getenv("DB_URL")}",
+        System.getenv("DB_USERNAME"),
+        System.getenv("DB_PASSWORD")
+    )
 
     fun popToken(token: String): Long? {
-        val currentTime = System.currentTimeMillis() / 1000L
+        getDbConnection().use { dbConnection ->
+            val currentTime = System.currentTimeMillis() / 1000L
 
-        //language=MySQL
-        val dbResult = getDatabaseResult("SELECT UserId, CreationDate FROM Tokens WHERE Token=?", Token, token)
-        val tokenResult = dbResult?.firstOrNull() ?: return null
-
-        //If the token is from last week
-        return if (currentTime - tokenResult.timeSeconds < 604800L) tokenResult.userId else {
             //language=MySQL
-            updateDatabase("DELETE FROM Tokens WHERE Token=?", token)
-            null
+            val dbResult =
+                dbConnection.getDatabaseResult("SELECT UserId, CreationDate FROM Tokens WHERE Token=?", Token, token)
+            val tokenResult = dbResult?.firstOrNull() ?: return null
+
+            //If the token is from last week
+            return if (currentTime - tokenResult.timeSeconds < 604800L) tokenResult.userId else {
+                //language=MySQL
+                dbConnection.updateDatabase("DELETE FROM Tokens WHERE Token=?", token)
+                null
+            }
         }
     }
 
     @Language("MySQL")
-    fun getUserById(id: Long): User? = getDatabaseResult("SELECT UserId, CurrentSemester, Major, IsAdmin FROM Users WHERE UserId=?", User, id)?.firstOrNull()
+    fun getUserById(id: Long): User? = getDbConnection().use {
+        it.getDatabaseResult(
+            "SELECT UserId, CurrentSemester, Major, IsAdmin FROM Users WHERE UserId=?",
+            User,
+            id
+        )?.firstOrNull()
+    }
 
     @Language("MySQL")
-    fun getCourses(): Set<Course> = getDatabaseResult("""
-        SELECT Courses.CourseId, Subject, Module, Course, Shorthand, Count(Users.UserId) AS UserCount FROM Courses
-        LEFT OUTER JOIN User_Course_Links UCL on Courses.CourseId = UCL.CourseId
-        LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
-        GROUP BY Courses.CourseId
-    """.trimIndent(), Course) ?: emptySet()
+    fun getCourses(): Set<Course> = getDbConnection().use {
+        it.getDatabaseResult(
+            """
+                SELECT Courses.CourseId, Subject, Module, Course, Shorthand, Count(Users.UserId) AS UserCount FROM Courses
+                LEFT OUTER JOIN User_Course_Links UCL on Courses.CourseId = UCL.CourseId
+                LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
+                GROUP BY Courses.CourseId
+            """.trimIndent(), Course
+        ) ?: emptySet()
+    }
 
     fun getCoursesForUser(userId: Long): Set<Course> {
-        //language=MySQL
-        updateDatabase("DROP TABLE IF EXISTS TempCourses")
-        //language=MySQL
-        updateDatabase("""
-            CREATE TEMPORARY TABLE TempCourses AS
-              SELECT Courses.CourseId, Subject, Module, Course, Shorthand, Count(Users.UserId) AS UserCount FROM Courses
-              LEFT OUTER JOIN User_Course_Links UCL on Courses.CourseId = UCL.CourseId
-              LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
-              GROUP BY Courses.CourseId;
-        """.trimIndent())
-        val getCoursesQuery = """
-            SELECT TempCourses.CourseId, Subject, Module, Course, Shorthand, UserCount FROM TempCourses
-            LEFT OUTER JOIN User_Course_Links UCL on TempCourses.CourseId = UCL.CourseId
-            LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
-            WHERE Users.UserId = ?
-        """.trimIndent()
-        return getDatabaseResult(getCoursesQuery, Course, userId) ?: emptySet()
+        getDbConnection().use {
+            //language=MySQL
+            it.updateDatabase("DROP TABLE IF EXISTS TempCourses")
+            //language=MySQL
+            it.updateDatabase(
+                """
+                    CREATE TEMPORARY TABLE TempCourses AS
+                      SELECT Courses.CourseId, Subject, Module, Course, Shorthand, Count(Users.UserId) AS UserCount FROM Courses
+                      LEFT OUTER JOIN User_Course_Links UCL on Courses.CourseId = UCL.CourseId
+                      LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
+                      GROUP BY Courses.CourseId;
+                """.trimIndent()
+            )
+            val getCoursesQuery = """
+                    SELECT TempCourses.CourseId, Subject, Module, Course, Shorthand, UserCount FROM TempCourses
+                    LEFT OUTER JOIN User_Course_Links UCL on TempCourses.CourseId = UCL.CourseId
+                    LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
+                    WHERE Users.UserId = ?
+                """.trimIndent()
+            return it.getDatabaseResult(getCoursesQuery, Course, userId) ?: emptySet()
+        }
     }
 
     fun updateSubscribedCourses(userId: Long, enabledCourses: Collection<Long>) {
-        //language=MySQL
-        updateDatabase("DELETE FROM User_Course_Links WHERE UserId=?", userId)
-        val enabledCoursesQuery = enabledCourses.joinToString(", ") { courseId -> "($userId, $courseId)" }
-        //language=MySQL
-        updateDatabase("INSERT INTO User_Course_Links (UserId, CourseId) VALUES $enabledCoursesQuery") //TODO: Convert to preparedStatements
+        getDbConnection().use {
+            //language=MySQL
+            it.updateDatabase("DELETE FROM User_Course_Links WHERE UserId=?", userId)
+            val enabledCoursesQuery = enabledCourses.joinToString(", ") { courseId -> "($userId, $courseId)" }
+            //language=MySQL
+            it.updateDatabase("INSERT INTO User_Course_Links (UserId, CourseId) VALUES $enabledCoursesQuery") //TODO: Convert to preparedStatements
+        }
     }
 
     fun updateUserInfo(id: Long, infoMap: Map<String, Any>) {
         val params = infoMap.map { "${it.key} = ${typeOrNull(it.value)}" }.joinToString(", ")
         //language=MySQL
-        updateDatabase("UPDATE Users SET $params WHERE UserId=?", id)
+        getDbConnection().use { it.updateDatabase("UPDATE Users SET $params WHERE UserId=?", id) }
     }
-    
+
     fun addCourse(subject: String, module: String?, course: String, shorthand: String?) {
         //language=MySQL
-        updateDatabase("INSERT INTO Courses (Subject, Module, Course, ShortHand) VALUES (?, ?, ?, ?)", subject, module, course, shorthand)
+        getDbConnection().use {
+            it.updateDatabase(
+                "INSERT INTO Courses (Subject, Module, Course, ShortHand) VALUES (?, ?, ?, ?)",
+                subject,
+                module,
+                course,
+                shorthand
+            )
+        }
     }
 
     @Language("MySQL")
     fun getCourseById(id: Long): Course? {
-        //language=MySQL
-        updateDatabase("DROP TABLE IF EXISTS TempCourses")
-        //language=MySQL
-        updateDatabase("""
-            CREATE TEMPORARY TABLE TempCourses AS
-              SELECT Courses.CourseId, Subject, Module, Course, Shorthand, Count(Users.UserId) AS UserCount FROM Courses
-              LEFT OUTER JOIN User_Course_Links UCL on Courses.CourseId = UCL.CourseId
-              LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
-              GROUP BY Courses.CourseId;
-        """.trimIndent())
-        val getCoursesQuery = """
-            SELECT TempCourses.CourseId, Subject, Module, Course, Shorthand, UserCount FROM TempCourses
-            WHERE TempCourses.CourseId=?
-        """.trimIndent()
-        return getDatabaseResult(getCoursesQuery, Course, id)?.firstOrNull()
+        getDbConnection().use {
+            //language=MySQL
+            it.updateDatabase("DROP TABLE IF EXISTS TempCourses")
+            //language=MySQL
+            it.updateDatabase(
+                """
+                    CREATE TEMPORARY TABLE TempCourses AS
+                      SELECT Courses.CourseId, Subject, Module, Course, Shorthand, Count(Users.UserId) AS UserCount FROM Courses
+                      LEFT OUTER JOIN User_Course_Links UCL on Courses.CourseId = UCL.CourseId
+                      LEFT OUTER JOIN Users on UCL.UserId = Users.UserId
+                      GROUP BY Courses.CourseId;
+                """.trimIndent()
+            )
+            val getCoursesQuery = """
+                    SELECT TempCourses.CourseId, Subject, Module, Course, Shorthand, UserCount FROM TempCourses
+                    WHERE TempCourses.CourseId=?
+                """.trimIndent()
+            return it.getDatabaseResult(getCoursesQuery, Course, id)?.firstOrNull()
+        }
     }
 
     @Language("MySQL")
-    fun getChannelsForCourse(id: Long): Set<Channel> = getDatabaseResult("SELECT ChannelId FROM Channels WHERE CourseId=?", Channel, id) ?: emptySet()
+    fun getChannelsForCourse(id: Long): Set<Channel> =
+        getDbConnection().use { it.getDatabaseResult("SELECT ChannelId FROM Channels WHERE CourseId=?", Channel, id) ?: emptySet() }
 
     fun generateTokenForUser(id: Long): String {
         val token = UUID.randomUUID().toString()
         val currentTime = System.currentTimeMillis() / 1000L
         //language=MySQL
-        updateDatabase("INSERT INTO Tokens (Token, UserId, CreationDate) VALUES ('$token', $id, $currentTime)")
+        getDbConnection().use { it.updateDatabase("INSERT INTO Tokens (Token, UserId, CreationDate) VALUES ('$token', $id, $currentTime)") }
         return token
     }
 
     @Language("MySQL")
-    fun getUsers(): Set<User> = getDatabaseResult("SELECT UserId, CurrentSemester, Major, IsAdmin FROM Users", User) ?: emptySet()
+    fun getUsers(): Set<User> =
+        getDbConnection().use { it.getDatabaseResult("SELECT UserId, CurrentSemester, Major, IsAdmin FROM Users", User) ?: emptySet() }
 
     fun addUser(user: User) {
         //language=MySQL
-        updateDatabase("INSERT INTO Users (UserId, CurrentSemester, Major) VALUES (?, ?, ?)", user.userId, user.currentSemester, user.mayor)
+        getDbConnection().use { it.updateDatabase(
+            "INSERT INTO Users (UserId, CurrentSemester, Major) VALUES (?, ?, ?)",
+            user.userId,
+            user.currentSemester,
+            user.mayor
+        ) }
     }
 
     @Language("MySQL")
-    fun getUsersForCourse(id: Long): Set<User> = getDatabaseResult("""
-        SELECT Users.UserId, CurrentSemester, Major, IsAdmin FROM Users
-        INNER JOIN User_Course_Links UCL on Users.UserId = UCL.UserId
-        WHERE UCL.CourseId=?
-    """.trimIndent(), User, id) ?: emptySet()
+    fun getUsersForCourse(id: Long): Set<User> = getDbConnection().use {
+        it.getDatabaseResult(
+            """
+                SELECT Users.UserId, CurrentSemester, Major, IsAdmin FROM Users
+                INNER JOIN User_Course_Links UCL on Users.UserId = UCL.UserId
+                WHERE UCL.CourseId=?
+            """.trimIndent(), User, id
+        ) ?: emptySet()
+    }
 
     fun addChannelToCourse(courseId: Long, channelId: Long) {
         //language=MySQL
-        updateDatabase("INSERT INTO Channels (ChannelId, CourseId) VALUES (?, ?)", channelId, courseId)
+        getDbConnection().use { it.updateDatabase("INSERT INTO Channels (ChannelId, CourseId) VALUES (?, ?)", channelId, courseId) }
     }
 
     @Language("MySQL")
-    fun isInDatabase(id: Long): Boolean = hasResult("SELECT * FROM Users WHERE UserId=?", id)
+    fun isInDatabase(id: Long): Boolean = getDbConnection().use { it.hasResult("SELECT * FROM Users WHERE UserId=?", id) }
 
     fun removeUser(id: Long) {
         //language=MySQL
-        updateDatabase("DELETE FROM Users WHERE UserId=?", id)
+        getDbConnection().use { it.updateDatabase("DELETE FROM Users WHERE UserId=?", id) }
     }
 
     fun removeCourse(id: Long) {
-        //language=MySQL
-        updateDatabase("DELETE FROM Channels WHERE CourseId=?", id)
-        //language=MySQL
-        updateDatabase("DELETE FROM User_Course_Links WHERE CourseId=?", id)
-        //language=MySQL
-        updateDatabase("DELETE FROM Courses WHERE CourseId=?", id)
+        getDbConnection().use {
+            //language=MySQL
+            it.updateDatabase("DELETE FROM Channels WHERE CourseId=?", id)
+            //language=MySQL
+            it.updateDatabase("DELETE FROM User_Course_Links WHERE CourseId=?", id)
+            //language=MySQL
+            it.updateDatabase("DELETE FROM Courses WHERE CourseId=?", id)
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
     fun updateCourseInfo(id: Long, map: Map<String, String?>) {
         val updateMap = map.filterValues { it != null && it != "" } as Map<String, String>
-        val update = updateMap.map { "${it.key} = ${typeOrNull(it.value)}" }.joinToString(", ") //TODO: Replace with PreparedStatements
+        val update = updateMap.map { "${it.key} = ${typeOrNull(it.value)}" }
+            .joinToString(", ") //TODO: Replace with PreparedStatements
         //language=MySQL
-        updateDatabase("UPDATE Courses SET $update WHERE CourseId=?", id)
+        getDbConnection().use { it.updateDatabase("UPDATE Courses SET $update WHERE CourseId=?", id) }
     }
 
     fun removeChannel(id: Long) {
         //language=MySQL
-        updateDatabase("DELETE FROM Channels WHERE ChannelId=?", id)
+        getDbConnection().use { it.updateDatabase("DELETE FROM Channels WHERE ChannelId=?", id) }
     }
 
     /*
@@ -179,37 +228,35 @@ class DatabaseHelper() {
         generateSequence { if (dbResult.next()) function(dbResult) else null }.toSet()
     }*/
 
-    private fun <T : Any> getDatabaseResult(query: String, factory: DatabaseItemInterface<T>, vararg args: Any?): Set<T>? = getOrNull(log) {
-        getDbConnection().use { dbConnection ->
-            val statement = dbConnection.prepareStatement(query).applyArgs(args)
-            log.trace(statement.toString())
-            val dbResult = statement.executeQuery()
-            return@getOrNull generateSequence { if (dbResult.next()) factory.generateItem(dbResult) else null }.toSet()
-        }
+    private fun <T : Any> Connection.getDatabaseResult(
+        query: String,
+        factory: DatabaseItemInterface<T>,
+        vararg args: Any?
+    ): Set<T>? = getOrNull(log) {
+        val statement = prepareStatement(query).applyArgs(args)
+        log.trace(statement.toString())
+        val dbResult = statement.executeQuery()
+        return@getOrNull generateSequence { if (dbResult.next()) factory.generateItem(dbResult) else null }.toSet()
     }
 
-    private fun hasResult(query: String, vararg args: Any?): Boolean = getOrNull(log) {
-        getDbConnection().use { dbConnection ->
-            val statement = dbConnection.prepareStatement(query).applyArgs(args)
-            log.trace(statement.toString())
-            return@getOrNull statement.executeQuery().next()
-        }
+    private fun Connection.hasResult(query: String, vararg args: Any?): Boolean = getOrNull(log) {
+        val statement = prepareStatement(query).applyArgs(args)
+        log.trace(statement.toString())
+        return@getOrNull statement.executeQuery().next()
     } ?: false
 
-    private fun updateDatabase(query: String, vararg args: Any?): Boolean = run {
-        getDbConnection().use { dbConnection ->
-            val result = getOrNull(log) {
-                val statement = dbConnection.prepareStatement(query).applyArgs(args)
-                log.trace(statement.toString())
-                statement.executeUpdate()
-            } ?: return@run false
-            return@run result > 0
-        }
+    private fun Connection.updateDatabase(query: String, vararg args: Any?): Boolean {
+        val result = getOrNull(log) {
+            val statement = prepareStatement(query).applyArgs(args)
+            log.trace(statement.toString())
+            statement.executeUpdate()
+        } ?: return false
+        return result > 0
     }
 
     private fun PreparedStatement.applyArgs(args: Array<out Any?>): PreparedStatement = apply {
         args.forEachIndexed { index, argument ->
-            when(argument) {
+            when (argument) {
                 null -> setNull(index + 1, Types.VARCHAR)
                 is Int -> setInt(index + 1, argument)
                 is Long -> setLong(index + 1, argument)
@@ -222,7 +269,11 @@ class DatabaseHelper() {
         }
     }
 
-    private fun <T> getOrNull(logger: Logger? = null, function: () -> T): T? = try { function() } catch (e: Exception) { (logger ?: log).info(e.toString()); null }
+    private fun <T> getOrNull(logger: Logger? = null, function: () -> T): T? = try {
+        function()
+    } catch (e: Exception) {
+        (logger ?: log).info(e.toString()); null
+    }
 
     interface DatabaseItemInterface<T> {
         fun generateItem(item: ResultSet): T
